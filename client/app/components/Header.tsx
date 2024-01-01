14b443e3d1cc5c86961707c1473fb7fd24211cfe
Orzu <orzu.maminov9@gmail.com>
2024-01-01 19:42:20 +0800

    hero

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -9,6 +9,9 @@ import CustomModal from "../utils/CustomModel";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/SignUp";
 import Verification from "../components/Auth/Verification";
+import { useSelector } from "react-redux";
+import Image from "next/image";
+import avatar from "../../public/assets/avatar.png"
 
 type Props = {
   open: boolean;
@@ -21,6 +24,7 @@ type Props = {
 const Header: FC<Props> = ({ activeItem, open, setOpen, route, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
+  const { user } = useSelector((state: any) => state.auth);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
@@ -69,11 +73,23 @@ const Header: FC<Props> = ({ activeItem, open, setOpen, route, setRoute }) => {
                   onClick={() => setOpenSidebar(true)}
                 />
               </div>
-              <HiOutlineUserCircle
-                size={25}
-                className="hidden 800px:block cursor-pointer dark:text-white text-black"
-                onClick={() => setOpen(true)}
-              />
+              {user ? (
+                <Link href={'/profile'}>
+                  <Image
+                    src={user.avatar ? user.avatar.url : avatar}
+                    alt=""
+                    width={30}
+                    height={30}
+                    className="w-[30px] h-[30px] rounded-full cursor-pointer"
+                  />
+                </Link>
+              ) : (
+                <HiOutlineUserCircle
+                  size={25}
+                  className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                  onClick={() => setOpen(true)}
+                />
+              )}
             </div>
           </div>
         </div>
--- client/redux/features/auth/authSlice.ts
+++ client/redux/features/auth/authSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { PayloadAction, createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
   token: "",
@@ -9,19 +9,19 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    userRegistration: (state, action) => {
+    userRegistration: (state, action: PayloadAction<{ token: string }>) => {
       state.token = action.payload.token;
     },
-    userLoggedIn: (state, action) => {
+    userLoggedIn: (state, action: PayloadAction<{ accessToken: string, user: string }>) => {
       state.token = action.payload.accessToken;
       state.user = action.payload.user;
     },
     userLoggedOut: (state) => {
       state.token = "";
       state.user = "";
-    }
-  }
-})
+    },
+  },
+});
 
 export const { userRegistration, userLoggedIn, userLoggedOut } = authSlice.actions;
 
